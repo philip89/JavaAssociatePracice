eb0c204b37b8decc5a47994f7453ccd32991d033
philip <pmurphy_89@eircom.net>
2018-12-04 19:39:06 +0000

    JavaBasics first commit

---
+++ WorkingWithDataTypes/src/ParseStringToLong.java
@@ -0,0 +1,28 @@
+public class ParseStringToLong{
+
+    public static void main(String[] args){
+
+
+        String mStr="123";
+        long m= new Long(mStr);
+        long m1 = Long.parseLong(mStr);
+        long m2 = Long.valueOf(mStr).longValue();
+
+
+        System.out.println(m);
+        System.out.println(m1);
+        System.out.println(m2);
+
+        // These dont work
+//        (new Long()).parseLong(mStr); Long does not have a no args constructor so cannot work
+//        Long.longValue(mStr) : longValue is a non-static method in Long class
+
+        //This is how to use longValue
+        Float i = new Float (3.0);
+        long b;
+
+        i.longValue();
+
+
+    }
+}
